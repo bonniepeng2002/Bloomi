100b91a07d6f077c043050f0ef621b95acd77cb8
chickenmilkcream <zhiting.peng@mail.utoronto.ca>
2020-12-30 21:43:11 -0400

    created Add a Plant Activity

--- app/src/main/java/com/bonniepeng/bloomi/AddPlant.java
+++ app/src/main/java/com/bonniepeng/bloomi/AddPlant.java
@@ -1,14 +1,125 @@
 package com.bonniepeng.bloomi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Calendar;
 
 public class AddPlant extends AppCompatActivity {
+
+    private TextView txtTitle,txtSciName,txtNickname,txtHeight,txtNotes;
+    private EditText edtName,edtNickname,edtHeight,edtNotes;
+    private Spinner notifsFrequency,metric;
+    private CheckBox cbNotifs;
+    private Button addToGarden,pickTime;
+    private ImageButton imageButton;
+    private static int notifHour, notifMinute;
+    private boolean notify;
+
+    //TODO: add metric options
+    //TODO: add frequency options
+    //TODO: set time differently: allow time to be seen and editted after they set it for a first time
+    // allow time to be seen after they set it
+    // allow toggle between having a time or not
+    //TODO: check for all required fields and proper data when clicking add to garden
+    //TODO: allow to change image when image button is clicked
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
+
+        instantiate();
+
+        // SETTING NOTIFS
+        cbNotifs.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (!cbNotifs.isChecked()){
+                    notify = false;
+                    pickTime.setVisibility(View.GONE);
+                    notifsFrequency.setVisibility(View.GONE);
+                } else {
+                    notify = true;
+                    pickTime.setVisibility(View.VISIBLE);
+                    notifsFrequency.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+
+    }
+
+    // LETTING USER PICK THE NOTIFICATION TIME
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+        @NotNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            notifHour = hourOfDay;
+            notifMinute = minute;
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    private void instantiate() {
+        // TEXTVIEWS
+        txtTitle = findViewById(R.id.txtTitle);
+        txtSciName = findViewById(R.id.txtPlantType);
+        txtNickname = findViewById(R.id.txtNickname);
+        txtHeight = findViewById(R.id.txtHeight);
+        txtNotes = findViewById(R.id.txtNotes);
+
+        // EDIT TEXTS
+        edtName = findViewById(R.id.edtPlantType);
+        edtNickname = findViewById(R.id.edtNickname);
+        edtHeight = findViewById(R.id.edtHeight);
+        edtNotes = findViewById(R.id.edtNotes);
+
+        // SPINNERS
+        notifsFrequency = findViewById(R.id.notifsFrequency);
+        metric = findViewById(R.id.metric);
+
+        // CHECKBOX
+        cbNotifs = findViewById(R.id.cbNotifs);
+
+        // BUTTONS
+        addToGarden = findViewById(R.id.btnAddToGarden);
+        imageButton = findViewById(R.id.imageButton);
+        pickTime = findViewById(R.id.btnTime);
     }
 }
---
+++ app/src/main/java/com/bonniepeng/bloomi/Plant.java
@@ -0,0 +1,89 @@
+package com.bonniepeng.bloomi;
+
+import android.media.Image;
+
+public class Plant {
+    private String sciName;
+    private String nickname;
+    private String metric;
+    private float height;
+    private String notes;
+    private String waterDays;
+    private String waterTime; //string??
+    private Image image;
+
+    public Plant(String sciName, String nickname, String metric, float height, String notes, String waterDays, String waterTime, Image image) {
+        this.sciName = sciName;
+        this.nickname = nickname;
+        this.metric = metric;
+        this.height = height;
+        this.notes = notes;
+        this.waterDays = waterDays;
+        this.waterTime = waterTime;
+        this.image = image;
+    }
+
+    public String getSciName() {
+        return sciName;
+    }
+
+    public void setSciName(String sciName) {
+        this.sciName = sciName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMetric() {
+        return metric;
+    }
+
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getWaterDays() {
+        return waterDays;
+    }
+
+    public void setWaterDays(String waterDays) {
+        this.waterDays = waterDays;
+    }
+
+    public String getWaterTime() {
+        return waterTime;
+    }
+
+    public void setWaterTime(String waterTime) {
+        this.waterTime = waterTime;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
